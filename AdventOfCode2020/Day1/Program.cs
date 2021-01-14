6e20b606c387ae7265d0e9ae05c824aad034740d
Tomas Oplatek <oplatek.t@gmail.com>
2021-01-14 18:29:58 +0100

    Add main project and Day1 solution

---
+++ AdventOfCode2020/Day1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using IOLibrary;
+
+namespace Day1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+
+            static void Part2()
+            {
+                var lines = FileHelper.GetLines("Day1");
+
+                var inputIntegers = lines.Select(int.Parse).ToList();
+                var numbersFound = false;
+
+                for (var i = 0; i < inputIntegers.Count; i++)
+                {
+                    for (var j = i + 1; j < inputIntegers.Count; j++)
+                    {
+                        for (var k = j + 1; k < inputIntegers.Count; k++)
+                        {
+                            if (inputIntegers[i] + inputIntegers[j] + inputIntegers[k] == 2020)
+                            {
+                                Console.WriteLine(inputIntegers[i] * inputIntegers[j] * inputIntegers[k]);
+                                numbersFound = true;
+                            }
+
+                            if (numbersFound) break;
+                        }
+
+                        if (numbersFound) break;
+                    }
+
+                    if (numbersFound) break;
+                }
+
+                if (!numbersFound) Console.WriteLine("No 3 numbers that add to 2020 found.");
+            }
+
+            static void Part1()
+            {
+                var lines = FileHelper.GetLines("Day1");
+
+                var inputIntegers = lines.Select(int.Parse).ToList();
+                var numbersFound = false;
+
+                for (var i = 0; i < inputIntegers.Count; i++)
+                {
+                    for (var j = i + 1; j < inputIntegers.Count; j++)
+                    {
+                        if (inputIntegers[i] + inputIntegers[j] == 2020)
+                        {
+                            Console.WriteLine(inputIntegers[i] * inputIntegers[j]);
+                            numbersFound = true;
+                        }
+
+                        if (numbersFound) break;
+                    }
+
+                    if (numbersFound) break;
+                }
+
+                if (!numbersFound) Console.WriteLine("No 2 numbers that add to 2020 found.");
+            }
+        }
+    }
+}
---
+++ AdventOfCode2020/IOLibrary/FileHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace IOLibrary
+{
+    public class FileHelper
+    {
+        private const string FileName = "PuzzleInput.txt";
+
+        public static IEnumerable<string> GetLines(string directory, string fileName = FileName)
+        {
+            var inputPath = Path.Combine(Environment.CurrentDirectory, $@"Data\{directory}\", fileName);
+
+            var lines = File.ReadLines(inputPath);
+
+            return lines;
+        }
+    }
+}
